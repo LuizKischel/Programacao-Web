642123a8d4db7937a429bee9b7e00ef696f18042
Your Name <you@example.com>
2021-03-10 21:32:13 -0300

    Noite1003

---
+++ Form10032021/script.js
@@ -0,0 +1,15 @@
+$(function(){
+    $(":input").css("backgroundColor", "yellow");
+    $(":text").css("backgroundColor", "white");
+    $(":password").css("backgroundColor", "gray");
+    $(":radio").parent().css("backgroundColor", "#bbb");
+    $(":checkbox").parent().css("backgroundColor", "#bbb");
+    $(":submit").css("backgroundColor", "#bbb");
+    $(":reset").css("backgroundColor", "#bbb");
+    $(":disabled").css("opacity", "0.5");
+    $(":checkbox:checked").parent().css("backgroundColor", "red");
+
+    $(".input option:selected").each(function() {
+        $(":text").val( $(this).text());
+     });
+});
