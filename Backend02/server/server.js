a1343cd3a173f6456b0904d913ec2baf6e85a4e3
Your Name <you@example.com>
2021-05-20 19:08:58 -0300

    sucess

--- Backend02/server/server.js
+++ Backend02/server/server.js
@@ -4,16 +4,12 @@ const booksRoute = require('./route/booksRoute');
 const clientsRoute = require('./route/clientsRoute');
 const purchasesRoute = require('./route/purchasesRoute');
 const companysRoute = require('./route/companysRoute');
-const swaggerAutogen = require('swagger-autogen')()
 
-const outputFile = './path/swagger-output.json'
 const endpointsFiles = ['./server/server.js']
 
 /* NOTE: if you use the express Router, you must pass in the 
    'endpointsFiles' only the root file where the route starts,
    such as: index.js, app.js, routes.js, ... */
-
-swaggerAutogen(outputFile, endpointsFiles)
 
 app.use(express.json());
 
